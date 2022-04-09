843dbe674a198528c3f6fa541aa09948c7d2bd15
Lucian Ciolac <lucian.ciolac@nagarro.com>
2022-04-09 10:00:25 +0300

    Angular-Assignment-2-Students-Starter-Code-2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product/product';
+import { products } from './product-list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shop';
+export class AppComponent implements OnInit {
+  public products: Product[] = [];
+
+  public ngOnInit(): void {
+    this.products = products;
+  }
 }
---
+++ src/app/product-list.ts
@@ -0,0 +1,60 @@
+import { Product } from "./product/product";
+
+export const products: Product[] = [
+  {
+      title: 'Iphone 5s',
+      price: 450,
+      description: 'Iphone 5s description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: true
+  },
+  {
+      title: 'Samsung s5',
+      price: 200,
+      description: 'Samsung s5 description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: false
+  },
+  {
+      title: 'Samsung s20',
+      price: 850,
+      description: 'Samsung s20 description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: false
+  },
+  {
+      title: 'Xiaomi MI',
+      price: 350,
+      description: 'Xiaomi MI description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: false
+  },
+  {
+      title: 'Xiaomi Note 10',
+      price: 550,
+      description: 'Xiaomi Note 10 description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: true
+  },
+  {
+      title: 'Sony Xperia',
+      price: 420,
+      description: 'Sony Xperia description',
+      isAvailable: false,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: false
+  },
+  {
+      title: 'Iphone X',
+      price: 799,
+      description: 'Iphone X description',
+      isAvailable: true,
+      imageLink: 'https://picsum.photos/200/300',
+      isPremium: true
+  }
+];
